536c49f108520a730d96e750acdf9dfd28faa5a3
codeBlooded1997 <52186148+codeBlooded1997@users.noreply.github.com>
2020-03-17 20:44:48 -0400

    pdf_url.py spider UPDATED

--- pdf_url/spiders/pdf_url.py
+++ pdf_url/spiders/pdf_url.py
@@ -6,23 +6,23 @@ from scrapy.spiders import CrawlSpider
 # scrapy.Spider (basic spider | Doesn't require rules method to define)
 class PdfUrlSpider(CrawlSpider):
 
-    # Neccesery
+    # This is required. It how I refer to this PdfUrlSpider class on the command line.
     name = 'pdf_url'
 
     # Optional (just doamin name no https:// required)
+    # Every link we look at MUST be a part of the adobe.com domain (i.e contain "adobe.com" in it's url)
     allowed_domains = ['adobe.com']
 
     # Url we want to start scraping from
     start_urls = ['https://www.adobe.com']
 
     # Required for CrawlSpider
-    # Rules that we have to follow for an specific link
-    # Any other link other than the alowed ones in Rule are going to be ignored (blank means all links are allowded)
-    # callback takes the fuction that we want to run on the extracted link
-    # follow means we are going to follow(recurse) every single link that we had extracted in the defined domain
+    # allow all links to be extracted
+    # call parse_httpresponse on each extracted link
+    # follow all links ("click on them") so we can check all the links on THAT webpage too
     rules = [Rule(LinkExtractor(allow=''), callback='parse_httpresponse', follow=True)]
 
-    # When ever we extarct the aloowed links we gonn asend it to this funcion to proccess
+    # When ever we extarct the allowed links we gonn asend it to this funcion to proccess
     def parse_httpresponse():
 
 
