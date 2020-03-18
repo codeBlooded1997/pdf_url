900e900d9c96f32701b696e1b35b0d68b765772e
codeBlooded1997 <52186148+codeBlooded1997@users.noreply.github.com>
2020-03-18 15:17:09 -0400

    Spider UPDATED

--- pdf_url/spiders/pdf_url.py
+++ pdf_url/spiders/pdf_url.py
@@ -16,48 +16,28 @@ class PdfUrlSpider(CrawlSpider):
 
     item = PdfUrlItem()
 
-    counter = 1
-
-
     def parse_httpresponse(self, response):
 
         # checking if url goes to pdf
         if b'Content-Type' in response.headers.keys():
-            goes_to_pdf = 'application/pdf' in str(response.headers['Content-Type'])
+            links_to_pdf = 'application/pdf' in str(response.headers['Content-Type'])
 
         else:
             return None
 
-
-        # Scraping data
-        if goes_to_pdf:
-            print(counter, 'PDF Link Found')
-            counter += 1
+        # If it does Scrape data
+        if links_to_pdf:
+            print('PDF Link Found')
             print(response.url)
             print(str(response.url).split('/')[-1])
-            print(goes_to_pdf)
+            print(links_to_pdf)
             print()
-
             item['filename'] = response.url.split('/')[-1]
             item['url'] = response.url
 
-        else:
-            pass
-
-
-        #if b'Content-Type' in response.headers.keys():
-        #    links_to_pdf = 'application/pdf' in str(response.headers['Content-Type'])
-        #else:
-        #    return None
-
-        # if it does, scrape it
-        #if links_to_pdf:
-        #    item['filename'] = response.url.split('/')[-1]
-        #    item['url'] = response.url
         # if not, ignore it and move on to the next link
-        #else:
-        #    return None
-
+        else:
+            return None
 
         # write that data to the csv
         return
